97553981591ad5e0359aa377497fa4adf72ab423
tobiasuihlein <tobias.uihlein@gmail.com>
2024-12-23 10:53:47 +0100

    use explore view for home screen

--- recipe_app/recipe_app/urls.py
+++ recipe_app/recipe_app/urls.py
@@ -4,13 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from . import views
+from recipes.views import ExploreView
 
 app_name = "recipe_app"
 
 urlpatterns = [
     path('api/', include("api.urls")),
     path('recipes/', include("recipes.urls")),
-    path('', views.home, name="home"),
+    path('', ExploreView.as_view(), name="home"),
     path('generator/', include("generator.urls")),
     path('admin/', admin.site.urls),
     path('chefs/', include("chefs.urls")),
